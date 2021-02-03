249d6a8bf667b1e603a06b866bc61d908154743b
Chance Lane <lanecw@gmail.com>
2021-02-03 14:51:57 -0500

    repair of the merge consflicts

--- scripts/main.js
+++ scripts/main.js
@@ -1,14 +1,15 @@
 import { useParkNames } from "./parks/ParkProvider.js"
 import { ParkSelect, render } from "./parks/ParkSelect.js"
+import { attractionName } from "./attractions/AttractionsSelect.js"
 
 useParkNames()
 ParkSelect()
-// import { } from "./parks/ParkSelect/js"
-
-// render()
-
-import { attractionName } from "./attractions/AttractionsSelect.js"
 attractionName()
-
-//import { AttractionList } from "./attractions/AttractionsSelect.js"
 AttractionList()
+
+
+
+
+
+
+
