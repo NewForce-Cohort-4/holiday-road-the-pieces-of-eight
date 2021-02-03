aa53c3e36392554626847bbedf5978d50e4144ce
Chance Lane <lanecw@gmail.com>
2021-02-03 14:43:43 -0500

    1

--- scripts/parks/ParkProvider.js
+++ scripts/parks/ParkProvider.js
@@ -11,7 +11,7 @@ let parkNames = []
 
  export const getParks = () => {
     // Request the data
-    return fetch("https://developer.nps.gov/api/v1/parks?limit=600&api_key=9wzNjNUr0KXvTztf6tOaT3XRKOVCcq0sPLZ3Im9x")
+    return fetch("https://developer.nps.gov/api/v1/parks?limit=600&api_")
        // Convert the JSON string reposne to a Javascript data structure (object or array)
        .then(response => response.json())
        // Do something with the data
