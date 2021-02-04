c8726123938d37c2ead78ae2b750affd804e0c48
Sophia <soapsstone@gmail.com>
2021-02-04 11:29:02 -0500

    ks new new updates

--- scripts/attractions/AttractionList.js
+++ scripts/attractions/AttractionList.js
@@ -3,7 +3,7 @@ import { useAttractions } from "./AttractionProvider"
 document.querySelector("body").addEventListener("click", (EventObject) => {
     console.log("you clicked me")
     console.log(EventObject.target.id)
-    if(EventObject.target.id.includes("attractions--")){
+    if(EventObject.target.id.includes("attractions-details")){
         const idOfAttractionClicked = (EventObject.target.id.split("--").pop())
         const allTheAttractions = useAttractions()
         
