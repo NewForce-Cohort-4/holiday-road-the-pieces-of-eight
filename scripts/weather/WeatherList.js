7103e1c3b6fd324e66bf9ff8ed8fead75c518206
Chance Lane <lanecw@gmail.com>
2021-02-05 09:25:46 -0500

    working on weather, pause for pr

--- scripts/weather/WeatherList.js
+++ scripts/weather/WeatherList.js
@@ -5,23 +5,25 @@ const weatherHtmlTarget = document.querySelector(".weather-container")
 export const weatherPrinter = () => {
     // fetch call and then return array of objects that represent parks
     getWeather().then(() => {
+        let weatherString=''
         const dailyWeatherArr = useDailyWeather()
+        let it = {}
+        for(let i=0; i<5; i++){
         
-        for(let i=0; i<4; i++){
-
-        let it = {
+         it[i] = {
             date: new Date(dailyWeatherArr[i].dt*1000),
             high: dailyWeatherArr[i].temp.max,
             low: dailyWeatherArr[i].temp.min,
             forecast: dailyWeatherArr[i].weather[0].description,
+            
+        } 
+          console.log(it);
         }
-        console.log(it);
-       }  
+    
     })
+
+
 }
-
-
-
 
 
 
